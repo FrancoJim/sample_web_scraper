f56a129f8c371afde0e9cf825effd256c01be6dd
Kevin J <cannedenthusiasm@users.noreply.github.com>
2018-10-29 13:04:28 -0400

    Grab Web Scrape Method

--- google_scraper/Grab_method.py
+++ google_scraper/Grab_method.py
@@ -1,7 +1,17 @@
 #!/bin/env python3
 
+'''
+Web Scraping using Grab Library.
+
+[Grab Documentation](https://grablib.org/en/latest/)
+
+NOTE: Library appears to have an issue with version of "six" module.
+(2018-10-29)
+
+
+'''
 
 from grab import Grab
 
 g = Grab()
-resp = g.go('http://livejournal.com/')
+resp = g.go('http://www.google.com/')
