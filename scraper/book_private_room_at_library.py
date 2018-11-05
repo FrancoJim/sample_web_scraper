b5b165ce4c7185e8fdfbcf5f8edb3968780a50f4
Kevin J. Lang <k.j.lang.tech@gmail.com>
2018-11-05 13:20:40 -0500

    Initial Upload

---
+++ scraper/book_private_room_at_library.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+'''
+book_private_room_at_library: Example app that to books a private room at a library that happens to be in Massachusetts.
+
+
+'''
+
+if __name__ == '__main__':
+    pass
